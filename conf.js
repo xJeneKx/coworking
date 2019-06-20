59d8aa355f07b5f001bcdb680e6ffe1e3b273766
xjenek <xxjenekxx@gmail.com>
2019-06-20 20:45:56 +0200

    change deviceName

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ exports.bLight = true;
 exports.storage = 'sqlite';
 
 exports.hub = 'obyte.org/bb-test';
-exports.deviceName = 'Test device address';
+exports.deviceName = 'Coworking(connectory)';
 exports.permanent_pairing_secret = 'test';
 exports.KEYS_FILENAME = 'keys.json';
 exports.TIMESTAMPER_ADDRESS = 'OPNUXBRSSQQGHKQNEPD2GLWQYEUY5XLD'; // isTestnet ? 'OPNUXBRSSQQGHKQNEPD2GLWQYEUY5XLD' : 'I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT'
