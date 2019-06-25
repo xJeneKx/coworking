b40d57059800cc6a2c0741c7ffdf894ff260fd88
xjenek <xxjenekxx@gmail.com>
2019-06-25 08:45:55 +0200

    test led

--- index.js
+++ index.js
@@ -44,7 +44,23 @@ let gpio17 = gpio.export(17, {
 	// read or write to the header right away. Place your logic in this ready
 	// function to guarantee everything will get fired properly
 	ready: function() {
-		gpio17.set(0);
+		gpio17.set(1);
+	}
+});// Calling export with a pin number will export that header and return a gpio header instance
+let gpio27 = gpio.export(27, {
+	// When you export a pin, the default direction is out. This allows you to set
+	// the pin value to either LOW or HIGH (3.3V) from your program.
+	direction: gpio.DIRECTION.OUT,
+	
+	// set the time interval (ms) between each read when watching for value changes
+	// note: this is default to 100, setting value too low will cause high CPU usage
+	interval: 200,
+	
+	// Due to the asynchronous nature of exporting a header, you may not be able to
+	// read or write to the header right away. Place your logic in this ready
+	// function to guarantee everything will get fired properly
+	ready: function() {
+		gpio27.set(1);
 	}
 });
 
